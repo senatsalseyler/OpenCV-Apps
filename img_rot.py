98fd67c5e97ff1cf4502544e73fbe945adfbad2a
senatsalseyler <79267854+senatsalseyler@users.noreply.github.com>
2023-06-01 16:15:35 +0300

    image rotation-2

---
+++ img_rot.py
@@ -0,0 +1,12 @@
+import cv2
+import numpy as np
+
+img = cv2.imread("C:/Users/MSI/Desktop/cizim/ps/botanik2.png")
+
+num_rows, num_cols = img.shape[:2]
+
+rotation_matrix = cv2.getRotationMatrix2D((num_rows/2, num_rows/2),30,1)
+img_rotation= cv2.warpAffine(img, rotation_matrix, (num_cols, num_rows))
+
+cv2.imshow("Rotation",img_rotation)
+cv2.waitKey()
